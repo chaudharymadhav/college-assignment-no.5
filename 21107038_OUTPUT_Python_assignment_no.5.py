c7683e3ca0e611c2fad76a593f5c2e8bc7529d05
MADHAV <104258321+chaudharymadhav@users.noreply.github.com>
2022-06-14 13:39:33 +0530

    Add files via upload

---
+++ 21107038_OUTPUT_Python_assignment_no.5.py
@@ -0,0 +1,140 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: D:\python\Assignment\Python assignment no.5.py ===========
+Enter a string-:MY NMAE IS MADHAV
+The original string  is :  MY NMAE IS MADHAV
+The reversed string using a while loop is :  VAHDAM SI EAMN YM
+
+Enter your first no.-:0
+Enter your second no.-:100
+Enter the number which you will divide on entire range-:9
+All numbers in range (0, 100) which are divisible by 9 are as follows-:
+0
+9
+18
+27
+36
+45
+54
+63
+72
+81
+90
+99
+
+Enter your first side:-1
+Enter your second side:-2
+Enter your third side:-3
+The sides entered don't for a triangle!!!
+Enter your first side:-2
+Enter your second side:-3
+Enter your third side:-4
+Area is equal to :- 2.9047375096555625
+Enter your first side:-3
+Enter your second side:-4
+Enter your third side:-5
+Area is equal to :- 6.0
+Enter your first side:-4
+Enter your second side:-5
+Enter your third side:-6
+Area is equal to :- 9.921567416492215
+Enter your first side:-5
+Enter your second side:-6
+Enter your third side:-7
+Area is equal to :- 14.696938456699069
+you were having only 5 turns to enter the sides of your triangle!!!
+
+The given pattern is as follows-:
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+Enter the number of rows: 10
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYABC
+DEFGHIJK
+LMNOPQRST
+UVWXYABCDE
+
+please,Enter the range in which you want to get prime number
+Enter the Lowest Range Value-:0
+Enter the Upper Range Value-:100
+The Prime Numbers in range  (0, 100) are as follows-: 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+
+Following are the numbers which are multiple of 7 and divisible by 11 in the range (1-500)-:
+77
+154
+231
+308
+385
+462
+
+you have to Enter 10 integers of your choice-:
+Enter an integer-:1
+Enter an integer-:2
+Enter an integer-:-4
+Enter an integer-:-9
+Enter an integer-:0
+Enter an integer-:8
+Enter an integer-:-65
+Enter an integer-:38
+Enter an integer-:2
+Enter an integer-:2
+This is the list of integers entered by the user-: [1, 2, -4, -9, 0, 8, -65, 38, 2, 2]
+positive numbers are as follows-:1,2,8,38,2,2,
+negative numbers are as follows-:-4,-9,-65,
+odd numbers are as follows-:1,
+even numbers are as follows-:2,8,38,2,2,
+Enter the number you want to count in list1-:2
+The number - 2 , occurs 3 times in the list1
+
+you can enter 10 words in list_enter
+Enter the word you want to add in the list_enter-:Madhav
+Enter the word you want to add in the list_enter-:keshav
+Enter the word you want to add in the list_enter-:madhav
+Enter the word you want to add in the list_enter-:Madhav
+Enter the word you want to add in the list_enter-:aditya
+Enter the word you want to add in the list_enter-:ayush
+Enter the word you want to add in the list_enter-:ayush
+Enter the word you want to add in the list_enter-:Madhav
+Enter the word you want to add in the list_enter-:aniket
+Enter the word you want to add in the list_enter-:aditi
+Enter the word you want to count in list_enter-:ishita
+word - ishita , occurs 0 times
